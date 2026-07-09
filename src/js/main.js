e793b2795c25e1194676e01a4f9d98b087d241b1
thneri95 <tiago.neri1995@outlook.com>
2026-07-09 15:34:22 -0300

    Week02 Completed

--- src/js/main.js
+++
@@ -1,3 +0,0 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
-import ProductData from "./Product.mjs";
--- src/js/product.js
+++ src/js/product.js
@@ -2,6 +2,12 @@ import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
+// Export the function for testing
+export function normalizeCartItems(items) {
+  // Your implementation here
+  return items;
+}
+
 const productId = getParam("product");
 const dataSource = new ProductData("tents");
 
